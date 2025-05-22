df7bccefaf96c3b87a79c146b3f6fc1d7fd0cb2e
Enzo <enzo.galiano38@gmail.com>
2025-05-22 19:47:30 +0200

    v2.7

--- src/js/dataProjects.js
+++ src/js/dataProjects.js
@@ -12,13 +12,13 @@ export default [
   {
     title: "Portfolio",
     slug: "portfolio",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    description: "Un projet personnel et profesionnel commencé et terminé en janvier 2024. Puis repris en debut 2025 dans le cadre d'un projet univairsitaire.",
     date:"2024",
     linkGit:"https://github.com/zNetrox/portfolio",
     nameImgFileTease:"portfolio-2024.png",
     listImgFile:createLimitedArray(2, 3, ["picture-poker.png", "memory.png", "tictactoe.png"]),
-    skills:["test", "test", "test", "test", "test"],
-    tools:["test", "test"]
+    skills:["Conception UX/UI", "Developpement Web", "Gestion De Projet"],
+    tools:["Affinity Designer", "Visual Studio Code", "Gsap", "Astro"]
   },
   {
     title: "Picture Poker",
@@ -29,50 +29,50 @@ export default [
     nameImgFileTease:"picture-poker.png",
     listImgFile:createLimitedArray(2, 3, ["picture-poker.png", "memory.png", "tictactoe.png"]),
     skills:["Conception UX/UI", "Programmation"],
-    tools:["HTML", "JavaScript", "CSS", "Affinity Designer"]
+    tools:["Affinity Designer", "HTML", "CSS", "JavaScript"]
   },
   {
     title: "Aesthetic",
     slug: "aesthetic",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    description: "Site web sur de la musique, éffectué en cours de SIN en terminal.",
     date:"2023",
     linkGit:"https://github.com/zNetrox/aesthetic",
     nameImgFileTease:"aesthetic.png",
     listImgFile:createLimitedArray(2, 3, ["picture-poker.png", "memory.png", "tictactoe.png"]),
-    skills:[],
-    tools:[]
+    skills:["Conception UX/UI", "Developpement Web", "Gestion De Projet"],
+    tools:["Affinity Designer", "HTML", "CSS", "JavaScript"]
   },
   {
     title: "Memory",
     slug: "memory",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    description: "Projet personnel sur la création d'un mini jeu video sur la mémorisation.",
     date:"2023",
     linkGit:"https://github.com/zNetrox/memory",
     nameImgFileTease:"memory.png",
     listImgFile:createLimitedArray(2, 3, ["picture-poker.png", "memory.png", "tictactoe.png"]),
-    skills:[],
-    tools:[]
+    skills:["Programmation", "Developpement Web"],
+    tools:["HTML", "CSS", "JavaScript"]
   },
   {
     title: "Tic-Tac-Toe",
     slug: "tic-tac-toe",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    description: "Projet personnel sur la création d'un mini jeu video sur de morpion.",
     date:"2023",
     linkGit:"https://github.com/zNetrox/tictactoe-pvp",
     nameImgFileTease:"tictactoe.png",
     listImgFile:createLimitedArray(2, 3, ["picture-poker.png", "memory.png", "tictactoe.png"]),
-    skills:[],
-    tools:[]
+    skills:["Programmation", "Developpement Web"],
+    tools:["HTML", "CSS", "JavaScript"]
   },
   {
     title: "Calculateur Age",
     slug: "calculateur-age",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    description: "projet personnel sur la création d'une application qui calcul l'age d'une personne jour pour jour.",
     date:"2023",
     linkGit:"https://github.com/zNetrox/age-calculator",
     nameImgFileTease:"age-calculator.png",
     listImgFile:createLimitedArray(2, 3, ["picture-poker.png", "memory.png", "tictactoe.png"]),
-    skills:[],
-    tools:[]
+    skills:["Programmation", "Developpement Web"],
+    tools:["HTML", "CSS", "JavaScript"]
   }
 ];
